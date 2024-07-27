6e2ec4577f9946549bb8f4e4c2301a5c5d17f215
yxpjseo <155122355+yxpjseo@users.noreply.github.com>
2024-07-27 19:21:01 +0900

    Title: 살 수 있는 물건, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240727/살 수 있는 물건/things-able-to-buy.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d",&n);
+    if(n>=3000)
+        printf("book");
+    else if(n>=1000)
+        printf("mask");
+    else
+        printf("no");
+    return 0;
+}
