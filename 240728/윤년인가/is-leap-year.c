ce568541a8a7ff7972cc47f9963d2c917a701754
yxpjseo <155122355+yxpjseo@users.noreply.github.com>
2024-07-28 20:19:56 +0900

    Title: 윤년인가, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240728/윤년인가/is-leap-year.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int y;
+    scanf("%d",&y);
+    if(y%4==0&&!(y%100==0&&y%400!=0))
+        printf("true");
+    else
+        printf("false");
+    return 0;
+}
