2d73a7f48722c7892df0cae4cfcedf5821b0924d
yxpjseo <155122355+yxpjseo@users.noreply.github.com>
2024-07-28 20:27:45 +0900

    Title: 일 수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240728/일 수 구하기/number-of-days-in-month.c
+++ 240728/일 수 구하기/number-of-days-in-month.c
@@ -6,15 +6,17 @@ int main() {
     scanf("%d",&n);
     if(n==2)
         printf("28");
-    else if(n<=7)
+    else if(n<=7){
         if(n%2==1)
             printf("31");
         else
         printf("30");
-    else
+    }  
+    else{
         if(n%2==0)
             printf("31");
         else
         printf("30");
+    }
     return 0;
 }
