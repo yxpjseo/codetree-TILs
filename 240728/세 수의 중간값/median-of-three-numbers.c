a92c202218d27584aa0b127dad19e4584bf02249
yxpjseo <155122355+yxpjseo@users.noreply.github.com>
2024-07-28 12:58:14 +0900

    Title: 세 수의 중간값, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240728/세 수의 중간값/median-of-three-numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if(b>a&&b<c)
+        printf("1");
+    else
+        printf("0");
+    return 0;
+}
