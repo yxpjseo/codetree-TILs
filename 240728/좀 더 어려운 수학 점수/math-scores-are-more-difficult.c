2ed4317f7fdbcec88237d1dc7bb7a35bb70e5767
yxpjseo <155122355+yxpjseo@users.noreply.github.com>
2024-07-28 15:32:12 +0900

    Title: 좀 더 어려운 수학 점수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240728/좀 더 어려운 수학 점수/math-scores-are-more-difficult.c
+++ 240728/좀 더 어려운 수학 점수/math-scores-are-more-difficult.c
@@ -4,17 +4,9 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int am,ae,bm,be;
     scanf("%d %d %d %d",&am,&ae,&bm,&be);
-    if(am!=bm){
-        if(am>bm)
-            printf("A");
-        else
-            printf("B");
-    }
-    else{
-        if(ae>be)
-            printf("A");
-        else
-            printf("B");
-    }
+    if(am>bm||am==bm&&ae>be)
+        printf("A");
+    else
+        printf("B");
     return 0;
 }
