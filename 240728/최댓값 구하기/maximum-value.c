af55cfe376d3178a994fc04b9cb381ba3a2a2913
yxpjseo <155122355+yxpjseo@users.noreply.github.com>
2024-07-28 20:32:45 +0900

    Title: 최댓값 구하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240728/최댓값 구하기/maximum-value.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if(a>=b){
+        if(a>=c)
+            printf("%d",a);
+        else //a<c
+            printf("%d",c);
+    }
+    else {//a<b
+        if(b>c)
+            printf("%d",b);
+        else //b<c
+            printf("%d",c);
+    }
+
+    return 0;
+}
