7be5bcf33ac8fb81c8f197b6052aaa021f80f4ea
yxpjseo <155122355+yxpjseo@users.noreply.github.com>
2024-07-28 15:27:20 +0900

    Title: 장학금, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

--- 240728/장학금/scholarship.c
+++ 240728/장학금/scholarship.c
@@ -4,13 +4,14 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int m,f;
     scanf("%d %d",&m,&f);
-    if (m>=90)
+    if (m>=90){
         if(f>=95)
             printf("100000");
         else if(f>=90)
             printf("50000");
         else
             printf("0");
+    }
     else
             printf("0");
 
