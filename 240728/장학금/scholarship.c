e22f3bf36590b76ae1eba5a4b62808f88273c996
yxpjseo <155122355+yxpjseo@users.noreply.github.com>
2024-07-28 15:23:27 +0900

    Title: 장학금, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240728/장학금/scholarship.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int m,f;
+    scanf("%d %d",&m,&f);
+    if (m>=95)
+        if(f>=95)
+            printf("100000");
+        else if(f>=90)
+            printf("50000");
+        else
+            printf("0");
+    else
+            printf("0");
+
+    return 0;
+}
