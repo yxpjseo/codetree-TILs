8c9d1db185773558949883d1ed8f45aa21a3961b
yxpjseo <155122355+yxpjseo@users.noreply.github.com>
2024-07-28 13:01:30 +0900

    Title: 숫자의 조건 여부 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240728/숫자의 조건 여부 3/number's-condition-3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    scanf("%d",&a);
+    if(a%13==0||a%19==0)
+        printf("True");
+    else
+        printf("False");
+    return 0;
+}
