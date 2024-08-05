c593ba6420ca99347467fdf7866ebd5f63045551
yxpjseo <155122355+yxpjseo@users.noreply.github.com>
2024-08-05 23:38:01 +0900

    Title: 총합 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240805/총합 구하기/find-the-total.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b,i,sum=0;
+    scanf("%d %d",&a,&b);
+    for(i=a;i<=b;i++){
+        if(i%6==0&&i%8!=0)
+            sum+=i;
+    }
+    printf("%d",sum);
+    return 0;
+}
