f6c351db744ff8509f8958a1a0553f62c11a7366
yxpjseo <155122355+yxpjseo@users.noreply.github.com>
2024-12-29 15:34:23 +0900

    Title: 열 단위로 반복하여 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241229/열 단위로 반복하여 출력/print-in-column.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            printf("%d",i+1);
+        }
+        printf("\n");
+    }
+    return 0;
+}
