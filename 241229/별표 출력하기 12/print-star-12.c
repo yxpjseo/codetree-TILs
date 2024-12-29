7702b3fc5965894f02c8be4ca74682c18ccf3528
yxpjseo <155122355+yxpjseo@users.noreply.github.com>
2024-12-29 15:22:50 +0900

    Title: 별표 출력하기 12, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241229/별표 출력하기 12/print-star-12.c
+++ 241229/별표 출력하기 12/print-star-12.c
@@ -4,9 +4,9 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     scanf("%d",&n);
-    for(int l=0; l<n; l++){
+    for(int l=0; l<n; l++)
         printf("* ");
-    }
+    printf("\n");
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
             if(j%2==0)
