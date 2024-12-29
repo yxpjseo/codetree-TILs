09920803306e0436961d5a56e015ee900f38b015
yxpjseo <155122355+yxpjseo@users.noreply.github.com>
2024-12-29 19:03:11 +0900

    Title: 정사각형 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241229/정사각형 출력/print-square.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int cnt=1, n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            printf("%d ",cnt);
+            cnt++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
