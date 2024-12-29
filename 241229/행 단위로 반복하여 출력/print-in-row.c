1dcaa764fcb19f6d6f6328407dc4199869698180
yxpjseo <155122355+yxpjseo@users.noreply.github.com>
2024-12-29 15:43:34 +0900

    Title: 행 단위로 반복하여 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241229/행 단위로 반복하여 출력/print-in-row.c
+++ 241229/행 단위로 반복하여 출력/print-in-row.c
@@ -2,5 +2,12 @@
 
 int main() {
     // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        for(int j=1;j<=n;j++)
+            printf("%d",j);
+        printf("\n");
+    }
     return 0;
 }
