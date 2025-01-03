ad89c6191141baa563e27cb6cc5f4521182d5d3a
yxpjseo <155122355+yxpjseo@users.noreply.github.com>
2025-01-03 13:03:58 +0900

    Title: 숫자 삼각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250103/숫자 삼각형/number-triangle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++)
+            printf("%d ",j+1);
+        printf("\n");
+    }
+    return 0;
+}
