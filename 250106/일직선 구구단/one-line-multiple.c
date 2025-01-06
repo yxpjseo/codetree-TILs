814b064f3ede2635d1aa503c44317eb24c7cede8
yxpjseo <155122355+yxpjseo@users.noreply.github.com>
2025-01-06 18:17:46 +0900

    Title: 일직선 구구단, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250106/일직선 구구단/one-line-multiple.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++)
+            printf("%d * %d = %d\n",i,j,i*j);
+    }
+    return 0;
+}
