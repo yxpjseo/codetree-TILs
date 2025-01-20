5f3a7f2ab23d3c6a39373278d55712af82cee6eb
yxpjseo <155122355+yxpjseo@users.noreply.github.com>
2025-01-20 21:34:39 +0900

    Title: 일의 자리 배열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250120/일의 자리 배열/array-with-units.c
+++ 250120/일의 자리 배열/array-with-units.c
@@ -2,11 +2,11 @@
 
 int main() {
     // Please write your code here.
-    int arr[12];
+    int a[12];
     scanf("%d %d",&a[0],&a[1]);
-    for(int i=2;i<12;i++){
+    for(int i=2;i<10;i++){
         a[i]=(a[i-1]+a[i-2])%10;
     }
-    for(int i=0;i<12;i++) printf("%d ",arr[i]);
+    for(int i=0;i<10;i++) printf("%d ",a[i]);
     return 0;
 }
