38f7afaf3e63d5f590fb2dfeb8effaa68139b829
Jinseo Yoo <155122355+yxpjseo@users.noreply.github.com>
2026-06-15 20:16:39 +0900

    [Passed] Time to Time (2ms, 0MB)

---
+++ trail2/Time to Time/time-to-time.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b, c, d;
+
+int main() {
+    cin >> a >> b >> c >> d;
+
+    // Please write your code here.
+    cout << (c*60+d) - (a*60+b);
+
+    return 0;
+}
